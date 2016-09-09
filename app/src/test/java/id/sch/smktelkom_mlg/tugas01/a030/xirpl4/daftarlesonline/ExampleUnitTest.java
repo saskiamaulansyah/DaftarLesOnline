e0c1100b2ca28a18a0e666c50316ef47cba7ba7c
SaskiaMaulansyah <saskiamaulansyah@gmail.com>
2016-09-09 16:45:29 +0700

    first commit : tampilan awal

---
+++ app/src/test/java/id/sch/smktelkom_mlg/tugas01/a030/xirpl4/daftarlesonline/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package id.sch.smktelkom_mlg.tugas01.a030.xirpl4.daftarlesonline;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
